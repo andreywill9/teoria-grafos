1312c57dd5bf624e8ce51fecfbe8951e62e4469f
andrey willians <andrey@hipcomp.com.br>
2021-03-30 20:46:31 -0300

    Atualização das querys de criação do banco automático

--- src/main/java/Connection/ServicoCidade.java
+++ src/main/java/Connection/ServicoCidade.java
@@ -2,18 +2,44 @@ package Connection;
 
 public class ServicoCidade {
 
-  private static final String CREATE_CIDADES = "CREATE TABLE IF NOT EXISTS `cidades` (\n" +
+  private static final String CREATE = "CREATE TABLE `cidades` (\n" +
+      "  `Id_cidade` int NOT NULL AUTO_INCREMENT,\n" +
       "  `nome_cidade` varchar(45) NOT NULL,\n" +
-      "  `sigla` varchar(45) NOT NULL,\n" +
-      "  `ativo` tinyint NOT NULL DEFAULT '1',\n" +
-      "  UNIQUE KEY `nome_cidade_UNIQUE` (`nome_cidade`),\n" +
+      "  `status` tinyint NOT NULL DEFAULT '1',\n" +
+      "  `sigla` varchar(3) NOT NULL,\n" +
+      "  PRIMARY KEY (`Id_cidade`),\n" +
+      "  UNIQUE KEY `Id_cidade_UNIQUE` (`Id_cidade`),\n" +
       "  UNIQUE KEY `sigla_UNIQUE` (`sigla`)\n" +
-      ")";
+      " )";
+
+  private static final String INSERT_PADRAO = "INSERT INTO cidades (nome_cidade, sigla) values\n" +
+      "\t('Porto Alegre','POA'),\n" +
+      "    ('Florianópolis','FLO'),\n" +
+      "    ('Blumenal','BLU'),\n" +
+      "    ('Curitiba','CUR'),\n" +
+      "    ('Londrina','LON'),\n" +
+      "    ('São Paulo','SPO'),\n" +
+      "    ('São José dos Campos','SJC'),\n" +
+      "    ('Rio de Janeiro','RJO'),\n" +
+      "    ('Belo Horizonte','BHO'),\n" +
+      "    ('Campinas','CMP'),\n" +
+      "    ('São José dos Campos','RBO'),\n" +
+      "    ('Bauru','BAU'),\n" +
+      "    ('Campo Grande','CPG'),\n" +
+      "    ('Cuiabá','CUI'),\n" +
+      "    ('Manaus','MAN'),\n" +
+      "    ('Belo Horizonte','BEL'),\n" +
+      "    ('Brasília','BSB'),\n" +
+      "    ('Natal','NTL'),\n" +
+      "    ('Recife','REC'),\n" +
+      "    ('Salvador','SLV')";
 
   private ConnectionFactory conexao;
 
   public ServicoCidade(ConnectionFactory conexao) throws Exception {
     this.conexao = conexao;
-    this.conexao.executar(CREATE_CIDADES);
+    this.conexao.executar(CREATE);
   }
+
+
 }
--- src/main/java/Connection/ServicoConexoes.java
+++ src/main/java/Connection/ServicoConexoes.java
@@ -2,20 +2,19 @@ package Connection;
 
 public class ServicoConexoes {
 
-  private static final String CREATE_CONEXOES = "CREATE TABLE  IF NOT EXISTS `conexoes` (\n" +
-      "  `origem` varchar(3) NOT NULL,\n" +
-      "  `destino` varchar(3) NOT NULL,\n" +
-      "  `saltos` int DEFAULT NULL,\n" +
-      "  `distancia` int DEFAULT NULL,\n" +
-      "  `custo` int DEFAULT NULL,\n" +
-      "  `id_conexao` varchar(7) NOT NULL,\n" +
-      "  `ativa` tinyint DEFAULT '1',\n" +
-      "  PRIMARY KEY (`id_conexao`),\n" +
-      "  UNIQUE KEY `idconexoes_UNIQUE` (`origem`),\n" +
-      "  UNIQUE KEY `conexoescol_UNIQUE` (`destino`),\n" +
-      "  UNIQUE KEY `id_conexao_UNIQUE` (`id_conexao`),\n" +
-      "  CONSTRAINT `destino` FOREIGN KEY (`destino`) REFERENCES `cidades` (`sigla`),\n" +
-      "  CONSTRAINT `origem` FOREIGN KEY (`origem`) REFERENCES `cidades` (`sigla`)\n" +
+  private static final String CREATE_CONEXOES = "CREATE TABLE `conexoes` (\n" +
+      "  `Id_conexao` int NOT NULL AUTO_INCREMENT,\n" +
+      "  `Id_cidade1` int NOT NULL,\n" +
+      "  `Id_cidade2` int NOT NULL,\n" +
+      "  `salto` int NOT NULL,\n" +
+      "  `distancia` int NOT NULL,\n" +
+      "  `custo` int NOT NULL,\n" +
+      "  `ativo` tinyint NOT NULL DEFAULT '1',\n" +
+      "  PRIMARY KEY (`Id_conexao`),\n" +
+      "  UNIQUE KEY `Id_cidade1_UNIQUE` (`Id_cidade1`),\n" +
+      "  UNIQUE KEY `Id_cidade2_UNIQUE` (`Id_cidade2`),\n" +
+      "  CONSTRAINT `Destino` FOREIGN KEY (`Id_cidade2`) REFERENCES `cidades` (`Id_cidade`),\n" +
+      "  CONSTRAINT `Origem` FOREIGN KEY (`Id_cidade1`) REFERENCES `cidades` (`Id_cidade`)\n" +
       ")";
 
   private ConnectionFactory conexao;
