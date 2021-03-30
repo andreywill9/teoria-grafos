e9b1fc80ebb8137b4181d345f1a20c653db18330
andrey willians <andrey@hipcomp.com.br>
2021-03-29 21:24:36 -0300

    Classe de conexão básica

--- src/main/java/Connection/ConnectionFactory.java
+++ src/main/java/Connection/ConnectionFactory.java
@@ -1,66 +1,48 @@
 package Connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
-/**
- *
- * @author joao
- */
 public class ConnectionFactory {
 
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";    
-    private static final String URL = "jdbc:mysql://localhost:3306/dbloja";    
-    private static final String USER = "root";    
-    private static final String PASS = "Pass1234";    
-    
-    public static Connection getConnection() {
-        try {
-            
-            Class.forName(DRIVER);
-            return DriverManager.getConnection(URL, USER, PASS);
-            
-        } catch (ClassNotFoundException | SQLException ex) {
-            
-            throw new RuntimeException("Erro na conexão:", ex);
-        }
+  private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+  private static final String URL = "jdbc:mysql://localhost:3306/NOME_BANCO";
+  private static final String USER = "USUARIO";
+  private static final String PASS = "SENHA";
+
+  private Connection conexao;
+
+  public ConnectionFactory() {
+    conexao = getConnection();
+  }
+
+  private Connection getConnection() {
+    try {
+      Class.forName(DRIVER);
+      return DriverManager.getConnection(URL, USER, PASS);
+    } catch (ClassNotFoundException | SQLException ex) {
+      throw new RuntimeException("Erro na conexão:", ex);
     }
-    
-    public static void closeConection(Connection con) {
-        if (con != null) {
-            try {
-                con.close();
-            } catch (SQLException ex) {
-                throw new RuntimeException("não foi possivel fechar à conexão", ex);
-            }
-        }
+  }
+
+  public ResultSet buscar(String query) throws Exception {
+    PreparedStatement ps = conexao.prepareStatement(query);
+    return ps.executeQuery();
+  }
+
+  public void executar(String query) throws Exception {
+    PreparedStatement ps = conexao.prepareStatement(query);
+    if (ps.executeUpdate() < 0) {
+      throw new Exception("Erro ao executar a query");
     }
-    
-    public static void closeConection(Connection con, PreparedStatement stmt) {
-        if (stmt != null) {
-            try {
-                stmt.close();
-            } catch (SQLException ex) {
-                throw new RuntimeException("não foi possivel fechar à conexão", ex);
-            }
-        }
-        
-        closeConection(con);
+  }
+
+  public void fecharConexao() throws SQLException {
+    if (!conexao.isClosed()) conexao.close();
+  }
+
+  public void abrirConexao() throws SQLException {
+    if (conexao.isClosed()) {
+      conexao = getConnection();
     }
-    
-    public static void closeConection(Connection con, PreparedStatement stmt,ResultSet rs) {
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (SQLException ex) {
-                throw new RuntimeException("não foi possivel fechar à conexão", ex);
-            }
-        }
-        
-  
-        closeConection(con, stmt);
-    }
+  }
 }
