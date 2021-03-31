223350e8a020a3be59627a4746abf327ef3a83d3
andrey willians <andrey@hipcomp.com.br>
2021-03-31 20:50:37 -0300

    Implementação de cadastrar conexões

--- src/main/java/model/Aresta.java
+++ src/main/java/model/Aresta.java
@@ -2,72 +2,58 @@ package model;
 
 public class Aresta {
 
-    private int distancia;
-    private int custo;
-    private Vertice origem;
-    private int hops;
-    private boolean ativa;
-    private Vertice destino;
+  private int distancia;
+  private int custo;
+  private Vertice origem;
+  private boolean ativa;
+  private Vertice destino;
 
-    public Aresta(int distancia, int custo, Vertice origem, Vertice destino) {
-        this.distancia = distancia;
-        this.custo = custo;
-        this.origem = origem;
-        this.destino = destino;
-        this.hops = 1;
-        this.ativa = true;
-    }
-    
-    //metodos de acesso
+  public Aresta(int distancia, int custo, Vertice origem, Vertice destino) {
+    this.distancia = distancia;
+    this.custo = custo;
+    this.origem = origem;
+    this.destino = destino;
+    this.ativa = true;
+  }
 
-    public int getHops() {
-        return hops;
-    }
+  public int getDistancia() {
+    return distancia;
+  }
 
-    public void setHops(int hops) {
-        this.hops = hops;
-    }
-    
-    
+  public void setDistancia(int distancia) {
+    this.distancia = distancia;
+  }
 
-    public int getDistancia() {
-        return distancia;
-    }
+  public int getCusto() {
+    return custo;
+  }
 
-    public void setDistancia(int distancia) {
-        this.distancia = distancia;
-    }
+  public void setCusto(int custo) {
+    this.custo = custo;
+  }
 
-    public int getCusto() {
-        return custo;
-    }
+  public Vertice getOrigem() {
+    return origem;
+  }
 
-    public void setCusto(int custo) {
-        this.custo = custo;
-    }
+  public void setOrigem(Vertice origem) {
+    this.origem = origem;
+  }
 
-    public Vertice getOrigem() {
-        return origem;
-    }
+  public Vertice getDestino() {
+    return destino;
+  }
 
-    public void setOrigem(Vertice origem) {
-        this.origem = origem;
-    }
+  public void setDestino(Vertice destino) {
+    this.destino = destino;
+  }
 
-    public Vertice getDestino() {
-        return destino;
-    }
+  public boolean isAtiva() {
+    return ativa;
+  }
 
-    public void setDestino(Vertice destino) {
-        this.destino = destino;
-    }
-
-    public boolean isAtiva() {
-        return ativa;
-    }
-
-    public void setAtiva(boolean ativa) {
-        this.ativa = ativa;
-    }
+  public void setAtiva(boolean ativa) {
+    this.ativa = ativa;
+  }
 
 }
