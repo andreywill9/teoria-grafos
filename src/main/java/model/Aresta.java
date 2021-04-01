007a2dc7985a080935000fa52925d6a086aac5c6
andrey willians <andrey@hipcomp.com.br>
2021-03-31 21:14:37 -0300

    Implementação da busca de conexões

--- src/main/java/model/Aresta.java
+++ src/main/java/model/Aresta.java
@@ -1,12 +1,27 @@
 package model;
 
+import java.sql.ResultSet;
+import java.util.Map;
+
 public class Aresta {
 
+  private int idConexao;
   private int distancia;
   private int custo;
   private Vertice origem;
   private boolean ativa;
   private Vertice destino;
+
+  public static Aresta instanciarDeResultSet(ResultSet rs, Map<Integer, Vertice> mapaCidades) throws Exception {
+    Aresta conexao = new Aresta();
+    conexao.setIdConexao(rs.getInt("Id_conexao"));
+    conexao.setOrigem(mapaCidades.get(rs.getInt("Id_cidade1")));
+    conexao.setDestino(mapaCidades.get(rs.getInt("Id_cidade2")));
+    conexao.setDistancia(rs.getInt("distancia"));
+    conexao.setCusto(rs.getInt("custo"));
+    conexao.setAtiva(rs.getBoolean("ativo"));
+    return conexao;
+  }
 
   public Aresta(int distancia, int custo, Vertice origem, Vertice destino) {
     this.distancia = distancia;
@@ -15,6 +30,8 @@ public class Aresta {
     this.destino = destino;
     this.ativa = true;
   }
+
+  public Aresta() {}
 
   public int getDistancia() {
     return distancia;
@@ -56,4 +73,11 @@ public class Aresta {
     this.ativa = ativa;
   }
 
+  public int getIdConexao() {
+    return idConexao;
+  }
+
+  public void setIdConexao(int idConexao) {
+    this.idConexao = idConexao;
+  }
 }
