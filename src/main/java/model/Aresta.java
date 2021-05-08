0e25465d631e7c2974bf61776b059f7dadb3bb52
andrey willians <andrey@hipcomp.com.br>
2021-05-08 15:21:21 -0300

    Comentários d edocumentação da classe aresta

--- src/main/java/model/Aresta.java
+++ src/main/java/model/Aresta.java
@@ -6,6 +6,13 @@ import java.sql.ResultSet;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * Classe responsável pelo objeto que representa uma aresta
+ * É um objeto que representa diretamente como os dados sao armazenados no banco de dados,
+ * tendo a funçao de fazer a ponte entre a Aresta usada na aplicaçao com o que e armazenado no banco
+ * Possui como atributo o seu id na tabela Conexoes, a medida de distancia, custo, se estaa se encontra ativa e os vertices
+ * que se encontram em cada extremidade
+ */
 public class Aresta {
 
   private int idConexao;
@@ -15,6 +22,14 @@ public class Aresta {
   private boolean ativa;
   private Vertice destino;
 
+  /**
+   * Metodo que instancia um novo objeto Aresta a partir dos dados contidos no banco
+   * @param rs representa o ResultSet em uma determinada posiçao para que seja buscado os atributos das colunas
+   * @param mapaCidades um HashMap com todas as cidades (vertices) tendo o id delas como chave, assim e  buscado a origem
+   *                    e destino a partir de seus ids
+   * @return um objeto Aresta
+   * @throws Exception se a linha atual do ResultSet for nula
+   */
   public static Aresta instanciarDeResultSet(ResultSet rs, Map<Integer, Vertice> mapaCidades) throws Exception {
     Aresta conexao = new Aresta();
     conexao.setIdConexao(rs.getInt("Id_conexao"));
@@ -26,6 +41,14 @@ public class Aresta {
     return conexao;
   }
 
+  /**
+   * Metodo que instancia um objeto Aresta a partir dos dados fornecidos pelo usuario
+   * @param cidade1 a origem da Aresta
+   * @param cidade2 o destino da Aresta
+   * @param distancia a medida de distancia das duas cidades
+   * @param custo o custo ao ir de uma cidade a outra
+   * @return um objeto Aresta
+   */
   public static Aresta instanciarNova(Vertice cidade1, Vertice cidade2, int distancia, int custo) {
     Aresta conexao = new Aresta();
     conexao.setOrigem(cidade1);
@@ -33,25 +56,6 @@ public class Aresta {
     conexao.setDistancia(distancia);
     conexao.setCusto(custo);
     return conexao;
-  }
-
-  public static Aresta instanciarInversa(Aresta aresta) {
-    Aresta novaAresta = new Aresta();
-    novaAresta.setIdConexao(aresta.getIdConexao());
-    novaAresta.setOrigem(aresta.getDestino());
-    novaAresta.setDestino(aresta.getOrigem());
-    novaAresta.setDistancia(aresta.getDistancia());
-    novaAresta.setCusto(aresta.getCusto());
-    novaAresta.setAtiva(aresta.isAtiva());
-    return novaAresta;
-  }
-
-  public Aresta(int distancia, int custo, Vertice origem, Vertice destino) {
-    this.distancia = distancia;
-    this.custo = custo;
-    this.origem = origem;
-    this.destino = destino;
-    this.ativa = true;
   }
 
   public Aresta() {}
@@ -108,6 +112,11 @@ public class Aresta {
     return ativa && getOrigem().getAtivo() && getDestino().getAtivo();
   }
 
+  /**
+   * Metodo que seleciona a metrica desejada da Aresta em questao
+   * @param metrica o tipo de metrica desejada, podendo ser distancia, salto ou custo
+   * @return um numero inteiro representando o valor da metrica desejada
+   */
   public int getPeso(MetricaCalculo metrica) {
     if (metrica == MetricaCalculo.SALTO) return 1;
     return metrica == MetricaCalculo.DISTANCIA ?
